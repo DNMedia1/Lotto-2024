97e91dd14e2ad9b593f493c2611132bbdc3fb33e
DNMedia1 <168175683+DNMedia1@users.noreply.github.com>
2024-11-27 23:44:36 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Lotto_2024
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtNumber1, txtNumber2, txtNumber3, txtNumber4, txtNumber5, txtNumber6;
+        private System.Windows.Forms.TextBox txtRandom1, txtRandom2, txtRandom3, txtRandom4, txtRandom5, txtRandom6;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblResult;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtNumber3 = new System.Windows.Forms.TextBox();
+            this.txtNumber4 = new System.Windows.Forms.TextBox();
+            this.txtNumber5 = new System.Windows.Forms.TextBox();
+            this.txtNumber6 = new System.Windows.Forms.TextBox();
+            this.txtRandom1 = new System.Windows.Forms.TextBox();
+            this.txtRandom2 = new System.Windows.Forms.TextBox();
+            this.txtRandom3 = new System.Windows.Forms.TextBox();
+            this.txtRandom4 = new System.Windows.Forms.TextBox();
+            this.txtRandom5 = new System.Windows.Forms.TextBox();
+            this.txtRandom6 = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber1.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber1.Location = new System.Drawing.Point(20, 20);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 29);
+            this.txtNumber1.TabIndex = 0;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber2.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber2.Location = new System.Drawing.Point(136, 20);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 29);
+            this.txtNumber2.TabIndex = 1;
+            // 
+            // txtNumber3
+            // 
+            this.txtNumber3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber3.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber3.Location = new System.Drawing.Point(256, 20);
+            this.txtNumber3.Name = "txtNumber3";
+            this.txtNumber3.Size = new System.Drawing.Size(100, 29);
+            this.txtNumber3.TabIndex = 2;
+            // 
+            // txtNumber4
+            // 
+            this.txtNumber4.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber4.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber4.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber4.Location = new System.Drawing.Point(377, 20);
+            this.txtNumber4.Name = "txtNumber4";
+            this.txtNumber4.Size = new System.Drawing.Size(100, 29);
+            this.txtNumber4.TabIndex = 3;
+            // 
+            // txtNumber5
+            // 
+            this.txtNumber5.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber5.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber5.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber5.Location = new System.Drawing.Point(490, 20);
+            this.txtNumber5.Name = "txtNumber5";
+            this.txtNumber5.Size = new System.Drawing.Size(105, 29);
+            this.txtNumber5.TabIndex = 4;
+            // 
+            // txtNumber6
+            // 
+            this.txtNumber6.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNumber6.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber6.ForeColor = System.Drawing.Color.Crimson;
+            this.txtNumber6.Location = new System.Drawing.Point(606, 20);
+            this.txtNumber6.Name = "txtNumber6";
+            this.txtNumber6.Size = new System.Drawing.Size(107, 29);
+            this.txtNumber6.TabIndex = 5;
+            // 
+            // txtRandom1
+            // 
+            this.txtRandom1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom1.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom1.Location = new System.Drawing.Point(20, 70);
+            this.txtRandom1.Name = "txtRandom1";
+            this.txtRandom1.ReadOnly = true;
+            this.txtRandom1.Size = new System.Drawing.Size(100, 29);
+            this.txtRandom1.TabIndex = 6;
+            // 
+            // txtRandom2
+            // 
+            this.txtRandom2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom2.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom2.Location = new System.Drawing.Point(136, 70);
+            this.txtRandom2.Name = "txtRandom2";
+            this.txtRandom2.ReadOnly = true;
+            this.txtRandom2.Size = new System.Drawing.Size(100, 29);
+            this.txtRandom2.TabIndex = 7;
+            // 
+            // txtRandom3
+            // 
+            this.txtRandom3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom3.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom3.Location = new System.Drawing.Point(256, 70);
+            this.txtRandom3.Name = "txtRandom3";
+            this.txtRandom3.ReadOnly = true;
+            this.txtRandom3.Size = new System.Drawing.Size(100, 29);
+            this.txtRandom3.TabIndex = 8;
+            // 
+            // txtRandom4
+            // 
+            this.txtRandom4.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom4.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom4.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom4.Location = new System.Drawing.Point(377, 70);
+            this.txtRandom4.Name = "txtRandom4";
+            this.txtRandom4.ReadOnly = true;
+            this.txtRandom4.Size = new System.Drawing.Size(100, 29);
+            this.txtRandom4.TabIndex = 9;
+            // 
+            // txtRandom5
+            // 
+            this.txtRandom5.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom5.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom5.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom5.Location = new System.Drawing.Point(490, 70);
+            this.txtRandom5.Name = "txtRandom5";
+            this.txtRandom5.ReadOnly = true;
+            this.txtRandom5.Size = new System.Drawing.Size(105, 29);
+            this.txtRandom5.TabIndex = 10;
+            // 
+            // txtRandom6
+            // 
+            this.txtRandom6.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtRandom6.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRandom6.ForeColor = System.Drawing.Color.Crimson;
+            this.txtRandom6.Location = new System.Drawing.Point(606, 70);
+            this.txtRandom6.Name = "txtRandom6";
+            this.txtRandom6.ReadOnly = true;
+            this.txtRandom6.Size = new System.Drawing.Size(107, 29);
+            this.txtRandom6.TabIndex = 11;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.Orange;
+            this.btnGenerate.Location = new System.Drawing.Point(20, 120);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(693, 30);
+            this.btnGenerate.TabIndex = 12;
+            this.btnGenerate.Text = "Zahlen überprüfen und generieren";
+            this.btnGenerate.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.Info;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Orange;
+            this.lblResult.Location = new System.Drawing.Point(20, 170);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(693, 30);
+            this.lblResult.TabIndex = 13;
+            this.lblResult.Text = "Ergebnis erscheint hier...";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = global::Lotto_2024.Properties.Resources.LOTTO6aus49_Spielinformation_1460x808;
+            this.ClientSize = new System.Drawing.Size(799, 581);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber3);
+            this.Controls.Add(this.txtNumber4);
+            this.Controls.Add(this.txtNumber5);
+            this.Controls.Add(this.txtNumber6);
+            this.Controls.Add(this.txtRandom1);
+            this.Controls.Add(this.txtRandom2);
+            this.Controls.Add(this.txtRandom3);
+            this.Controls.Add(this.txtRandom4);
+            this.Controls.Add(this.txtRandom5);
+            this.Controls.Add(this.txtRandom6);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblResult);
+            this.Name = "Form1";
+            this.Text = "Lotto 2024";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lotto_2024
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Spielerzahlen aus den Textfeldern lesen
+                int[] playerNumbers = new int[6]
+                {
+                    int.Parse(txtNumber1.Text),
+                    int.Parse(txtNumber2.Text),
+                    int.Parse(txtNumber3.Text),
+                    int.Parse(txtNumber4.Text),
+                    int.Parse(txtNumber5.Text),
+                    int.Parse(txtNumber6.Text)
+                };
+
+                // Validierung: Zahlen zwischen 1 und 49 prüfen
+                if (playerNumbers.Any(n => n < 1 || n > 49))
+                {
+                    MessageBox.Show("Alle Zahlen müssen zwischen 1 und 49 liegen!", "Ungültige Eingabe", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Validierung: Keine Duplikate zulassen
+                if (playerNumbers.Distinct().Count() != 6)
+                {
+                    MessageBox.Show("Die Zahlen dürfen sich nicht wiederholen!", "Ungültige Eingabe", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Zufallszahlen generieren (6 eindeutige Zahlen)
+                Random random = new Random();
+                int[] randomNumbers = Enumerable.Range(1, 49)
+                                                .OrderBy(_ => random.Next())
+                                                .Take(6)
+                                                .OrderBy(n => n)
+                                                .ToArray();
+
+                // Zufallszahlen in den Textboxen anzeigen
+                txtRandom1.Text = randomNumbers[0].ToString();
+                txtRandom2.Text = randomNumbers[1].ToString();
+                txtRandom3.Text = randomNumbers[2].ToString();
+                txtRandom4.Text = randomNumbers[3].ToString();
+                txtRandom5.Text = randomNumbers[4].ToString();
+                txtRandom6.Text = randomNumbers[5].ToString();
+
+                // Übereinstimmungen prüfen und anzeigen
+                var matchingNumbers = playerNumbers.Intersect(randomNumbers).ToArray();
+                lblResult.Text = matchingNumbers.Length > 0
+                    ? $"Übereinstimmungen: {string.Join(", ", matchingNumbers)}"
+                    : "Keine Übereinstimmungen.";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bitte geben Sie in allen Feldern gültige Zahlen ein!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
